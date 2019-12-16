b274e2833d2f457d8cc2e5e4a812280c403cd62f
Lucas <lukymassaro@gmail.com>
2019-12-16 14:29:53 -0300

    Modal básico agregado

--- main.js
+++ main.js
@@ -21,13 +21,57 @@ function agregarElemento(arreglo){
         div.style.backgroundColor= 'rgba(240, 240, 240, 0.6)';
         let h3= document.createElement("h3");
         let ahref = document.createElement('a');
-        ahref.href=name+"/index.html";
+        // ahref.href=name+"/index.html";
         ahref.textContent=name;
+        ahref.id = name;
+
+        //crear modal//
+
+        let modal = document.createElement('div');
+        modal.className = 'modal';
+        modal.id = 'modal'+name;
+        $(modal).html(
+            `<div class="modal-content" id=${name}>
+            <span class="close">&times;</span>
+                <h1>${name}</h1>
+                <p class="parrafo-modal">  </p>
+                <button><a href="${name}/index.html">Ir</a></button>
+            </div>
+            `
+        );
+        console.log(modal);
+        
+        // Get the button that opens the modal
+        var btn = $(ahref);
+        
+        // Get the <span> element that closes the modal
+        var close = $(modal).find(".close");
+        
+        // When the user clicks on the button, open the modal
+        $(btn).click(function(){
+            console.log(modal);
+            $(modal).fadeToggle(1400);
+        });
+        
+        // When the user clicks on <span> (x), close the modal
+        close.click(function() {
+            $(modal).fadeToggle(1400);
+        });
+        
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function() {
+          if (modal.style.display != none) {
+            modal.style.display = "none";
+          }
+        } 
+
+
         
         h3.appendChild(ahref);
         div.appendChild(h3);
+        div.appendChild(modal);
         //contenedor.appendChild(divFondo);
-        console.log(arreglo == arrayDeProyectos);
+        // console.log(arreglo == arrayDeProyectos);
         if (arreglo == arrayDeProyectos){
             console.log("hola")
             divPro.appendChild(divFondo);
@@ -47,3 +91,21 @@ arrayDeEjercicios.push('Calculadora')
 
 agregarElemento(arrayDeProyectos);
 agregarElemento(arrayDeEjercicios);
+
+
+
+let descripcion = $('#modalRompecabezas').find('.parrafo-modal');
+console.log(descripcion);
+$(descripcion).html(
+     "Descripción del proyecto Rompecabezas"
+ );
+
+descripcion = $('#modalPixel-Art').find('.parrafo-modal');
+$(descripcion).html(
+     "Descripción del proyecto Pixel-Art"
+ );
+
+ descripcion = $('#modalCalculadora').find('.parrafo-modal');
+$(descripcion).html(
+     "Descripción del proyecto Calculadora"
+ );
