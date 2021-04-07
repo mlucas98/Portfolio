a85055bb78c680111c251d22e8fc92aac15dd454
Lucas <lukymassaro@gmail.com>
2021-04-07 15:59:07 -0300

    Agregar landing de easybank y ajustar un poco boton de contacto

--- projects.js
+++ projects.js
@@ -9,6 +9,7 @@ function agregarElemento(arreglo) {
         $(divFondo).addClass(['p-1', 'col-md-4', 'fondoP'])
         let ahref = $('<a></a>');
         $(ahref).attr({ id: projectObject.name, href: projectObject.url, target: '_blank' });
+        // $(ahref).attr({ id: projectObject.name, hrEsabyBank Landing: projectObject.url, target: '_blank' });
         let imagen = $('<img>');
         $(imagen).attr('src', 'img/proyectos/' + projectObject.name + '.jpg');
         $(imagen).addClass(['d-block', 'w-100']);
@@ -31,9 +32,10 @@ arrayDeProyectos.push({ 'name': 'Pixel-Art', 'url': 'https://github.com/mlucas98
 arrayDeProyectos.push({ 'name': 'Rompecabezas', 'url': 'https://github.com/mlucas98/rompecabezas', 'description': 'Descripción del proyecto Rompecabezas' });
 arrayDeProyectos.push({ 'name': 'Testing', 'url': 'https://github.com/mlucas98/reservando', 'description': 'Descripción del proyecto de Testing' });
 arrayDeProyectos.push({ 'name': 'Encuestados', 'url': '#', 'description': 'El proyecto consiste en un programa de preguntas y respuestas aplicando el patrón MVC. Posee la vista tanto de Administrador como la de Usuario.' })
-arrayDeProyectos.push({ 'name': 'Que Veo Hoy', 'url': 'https://github.com/mlucas98/que-veo-hoy', 'description': 'Aplicación que recomienda qué película ver basandose en distintos filtros.' })
-arrayDeProyectos.push({ 'name': 'Peli vs Peli', 'url': 'https://github.com/mlucas98/peli-vs-peli', 'description': 'Aplicación que permite crear competencias entre películas de una base de datos local.' })
-arrayDeProyectos.push({ 'name': 'Dandelion', 'url': 'https://www.dandelionarte.com.ar/', 'description': 'Desarrollo de la web institucional de la escuela de comedia musical Dandelion.' })
+arrayDeProyectos.push({ 'name': 'Que Veo Hoy', 'url': 'https://github.com/mlucas98/que-veo-hoy', 'description': 'Aplicación que recomienda qué película ver basandose en distintos filtros.' });
+arrayDeProyectos.push({ 'name': 'Peli vs Peli', 'url': 'https://github.com/mlucas98/peli-vs-peli', 'description': 'Aplicación que permite crear competencias entre películas de una base de datos local.' });
+arrayDeProyectos.push({ 'name': 'Dandelion', 'url': 'https://www.dandelionarte.com.ar/', 'description': 'Desarrollo de la web institucional de la escuela de comedia musical Dandelion.' });
+arrayDeProyectos.push({ 'name': 'EasyBank Landing', 'url': 'https://mlucas98.github.io/easyBank-landing/', 'description': 'Landing page para un banco ficticio. Hecho con scss, html y js' });
 
 https: //github.com/mlucas98/que-veo-hoy
     agregarElemento(arrayDeProyectos);
